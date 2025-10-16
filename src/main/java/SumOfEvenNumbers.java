176a6abf4e914d6874cd5c0bcb20601cc9e9bd5b
03conner <03conner.chan@kamsc.org>
2025-10-16 13:12:19 -0400

    please doen fail me now!!!@!! I WANT KNOWE THE

--- src/main/java/SumOfEvenNumbers.java
+++ src/main/java/SumOfEvenNumbers.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 public class SumOfEvenNumbers {
     public static void main(String[] args) {     
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter an interger value");
+        System.out.println("Enter an integer value");
         int num = sc.nextInt();
-        if (num > 2){
+        int sum;
+        sum = 0;
+        if (num >= 2){
+            for(int i = 0; i <= num; i +=2){
 
+                    sum += i;
+
+            }
+            System.out.println("The sum of even numbers between 2 and " + num + " is " + sum);
         }
         else{
             System.out.println("Error: Input must be 2 or greater.");

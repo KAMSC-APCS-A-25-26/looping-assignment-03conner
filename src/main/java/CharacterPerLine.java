93315587208d30213a46bdae1bc6d8ce4a2aecbb
03conner <03conner.chan@kamsc.org>
2025-10-16 13:25:09 -0400

    micel mabrsd

--- src/main/java/CharacterPerLine.java
+++ src/main/java/CharacterPerLine.java
@@ -4,7 +4,7 @@ public class CharacterPerLine {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String cheese;
-        System.out.println("Enter a word: ");
+        System.out.print("Enter a word: ");
         cheese = sc.nextLine();
         for (int t = 0; t < cheese.length(); t ++){
             System.out.println(cheese.charAt(t));
